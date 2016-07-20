96a973bfc5e4e105895b7d7c767afb15e18e78af
mero <sergiy.1911@gmail.com>
2016-07-20 12:38:35 +0300

    Fixed bug in linking

--- create_links.sh
+++ create_links.sh
@@ -2,12 +2,12 @@
 
 rm ~/.tmux.conf
 rm  ~/.zshrc
-rm .bash_aliases
+rm ~/.bash_aliases
 rm ~/.vimrc
 rm  ~/init.vim
 
+ln -s ~/.vim/init.vim  ~/init.vim
 ln -s ~/.vim/.tmux.conf ~/.tmux.conf
 ln -s ~/.vim/.vimrc ~/.vimrc
-ln -s ~/.vim/init.vim  ~/init.vim
 ln -s ~/.vim/.zshrc  ~/.zshrc
-ln -s .vim/.bash_aliases .bash_aliases
+ln -s .vim/.bash_aliases ~/.bash_aliases
