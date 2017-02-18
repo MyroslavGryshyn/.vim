fa1027359b72385a57f3d15272fe01c868ebb917
Myroslav Hryshyn <sergiy.1911@gmail.com>
2017-02-18 16:45:58 +0000

    Added autosuggestion install

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 bash install_nvim.sh
 
+sudo apt-get install zsh
+
 # installing oh_my_zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
 sudo apt-get install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-brew install the_silver_searcher
 
 sudo add-apt-repository ppa:pgolm/the-silver-searcher
 sudo apt-get update
