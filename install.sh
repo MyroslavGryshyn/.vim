4202e907cce7f6f2211e4b0deb059afa099c6f2c
Mero <mhryshyn@gmail.com>
2023-07-20 18:49:20 +0000

    Make Ubuntu work

--- install.sh
+++ install.sh
@@ -8,10 +8,9 @@ git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shel
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-brew install the_silver_searcher
+sudo apt-get install silversearcher-ag
 
 bash create_links.sh
 
@@ -22,7 +21,4 @@ git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Fuzzy search
-brew install fzf
-
-# To install useful key bindings and fuzzy completion:
-$(brew --prefix)/opt/fzf/install
+sudo apt install fzf
--- install_nvim.sh
+++ install_nvim.sh
@@ -1,4 +1,6 @@
-brew install neovim
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
 
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 ln -s ~/.vim $XDG_CONFIG_HOME/nvim
@@ -6,5 +8,7 @@ ln -s ~/.vim/init.vim $XDG_CONFIG_HOME/nvim/init.vim
 ln -s ~/.vim/.shell_prompt.sh ~/
 ln -s ~/.vim/.tmux_airline_theme ~/
 
+sudo apt install ruby-dev
+sudo apt install npm
 sudo gem install neovim
 sudo npm install -g neovim
